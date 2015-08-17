83d5b56b2c399e946cdd1a62d3ec7717f4659e95
daraghmartin <d.martin@base2services.com>
2015-08-18 06:00:05 +1000

    added sample script for delete vols

---
+++ scripts/delete_unattached_vols.py
@@ -0,0 +1,11 @@
+import sys
+sys.path.append("alfajor")
+import boto
+from alfajor import aws_ec2
+
+account = "default"
+if len(sys.argv) < 2:
+      account = sys.argv[1]
+
+ec2 = aws_ec2.EC2(debug = True, verbose = True, account = account)
+ec2.delete_unattached_volumes()
