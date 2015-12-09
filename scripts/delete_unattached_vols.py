d156f032f4fd6e5851ee68d6eec2e17767c4696e
Nikola Pajtic <n.pajtic@base2services.com>
2015-12-10 09:45:11 +1000

    debugging

--- scripts/delete_unattached_vols.py
+++ scripts/delete_unattached_vols.py
@@ -14,4 +14,4 @@ if len(sys.argv) < 3:
 
 ec2 = aws_ec2.EC2(debug = True, verbose = True, account = account)
 #ec2.delete_unattached_volumes()
-ec2.delete_unattached_volumes_with_keeptag(volumekeeptag = volumekeeptag)
+ec2.delete_unattached_volumes_with_keeptag(volumekeeptag)
