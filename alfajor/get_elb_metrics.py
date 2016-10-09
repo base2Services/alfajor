8ae7e6f25532d9a40ebe6aa040471dc2962030f0
Nikola Pajtic <n.pajtic@base2services.com>
2016-10-10 08:51:29 +1100

    fix get_elb_metrics

--- alfajor/get_elb_metrics.py
+++ alfajor/get_elb_metrics.py
@@ -10,6 +10,7 @@ class ElbMetrics(AWS_BASE):
     def init(self):
         self.set_conn(boto.ec2.cloudwatch.connect_to_region(**self.get_connection_settings()))
 
+
     def get_elb_stats(self, name, metric, namespace, statistic, period=300, unit='Count'):
         try:
             stats = self.get_conn().get_metric_statistics(
@@ -26,7 +27,7 @@ class ElbMetrics(AWS_BASE):
             if not stats:
                 sum_of_req = 0.0
             else:
-                sum_of_req = (stats[0]['Sum'])
+                sum_of_req = (stats[0][statistic])
             current_value = int(round(sum_of_req))
             print current_value
         except BotoServerError, error:
