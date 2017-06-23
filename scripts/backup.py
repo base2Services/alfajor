08a616a3af8776b0cf0a2d85be137c14c2e1be06
toshke <n.tosic@base2services.com>
2017-06-23 12:44:39 +1000

    Sort out python path

--- scripts/backup.py
+++ scripts/backup.py
@@ -2,8 +2,9 @@ import sys
 import os
 
 pwd = os.environ['PWD']
-alfajor_path = "{0}".format(pwd)
-alfajor_path2 = "{0}/..".format(pwd)
+alfajor_path = "{0}".format(pwd) # if running from alfajor root
+alfajor_path2 = "{0}/..".format(pwd) #if running from scripts folder
+alfajor_path3 = "{0}/alfajor".format(pwd) #if running from folder above alfajor
 
 sys.path.append(alfajor_path)
 sys.path.append(alfajor_path2)
